49d4bdcc282a8fe233fd8490a5cc3e6f92447941
kamal Busari <busari1kamal@gmail.com>
2023-02-27 23:11:16 +0800

    WIP: Added crud operations for user api

--- routes/product.py
+++ routes/product.py
@@ -2,10 +2,11 @@ from fastapi import APIRouter
 from models.product import Product
 from config.db import conn
 from schemas.product import prodEntity, productsEntity
-
+from bson import objectid
 
 product = APIRouter()
+db = conn.ossyinventory.products
 
 @product.get('/products/')
 async def get_all_products():
-    return productsEntity(conn.ossyinventory.products.find())
+    return productsEntity(db.find())
--- routes/user.py
+++ routes/user.py
@@ -2,20 +2,37 @@ from fastapi import APIRouter
 from models.user import User
 from config.db import conn
 from schemas.user import userEntity, usersEntity
+from bson import objectid
 
 
 user = APIRouter()
+db = conn.ossyinventory.users
 
-@user.get('/users/')
+@user.get('/')
 async def get_all_users():
-    return usersEntity(conn.ossyinventory.users.find())
+    return usersEntity(db.find())
 
 
-@user.get('/users/limit=?{amt}')
+@user.get('/limit=?{amt}')
 async def get_limited_users(amt: int):
-    return usersEntity(conn.ossyinventory.users.find().limit(amt))
+    return usersEntity(db.find().limit(amt))
 
 
-@user.get('/user/{id}')
+@user.get('/{id}')
 async def get_single_user(id):
-    return userEntity(conn.ossyinventory.users.find_one({"id": id}))
+    return userEntity(db.find_one({"id": id}))
+
+@user.post('/')
+async def create_user(user: User):
+    db.insert_one(dict(user))
+    return userEntity(db.find_one({"name": user.name}))
+
+@user.put('/{id}')
+async def update_user(id, user: User):
+    db.find_one_and_update({"_id": objectid(id)},{"$set":dict(user)})
+    return userEntity(db.find_one({"_id": objectid(id)}))
+
+@user.delete('/{id}')
+async def delete_user(id, user: User):
+    db.find_one_and_delete({"_id": objectid(id)})
+    return {"message": "User deleted succesfully"}
