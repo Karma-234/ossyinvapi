b5dbabddfffb4f392b6835b5850fa68defe2f9e3
kamal Busari <busari1kamal@gmail.com>
2023-02-27 23:21:23 +0800

    WIP: Added crud operations for products

--- routes/product.py
+++ routes/product.py
@@ -10,3 +10,28 @@ db = conn.ossyinventory.products
 @product.get('/products/')
 async def get_all_products():
     return productsEntity(db.find())
+
+
+@product.get('/limit=?{amt}')
+async def get_limited_users(amt: int):
+    return productsEntity(db.find().limit(amt))
+
+
+@product.get('/{id}')
+async def get_single_user(id):
+    return prodEntity(db.find_one({"id": id}))
+
+@product.post('/')
+async def create_user(prod: Product):
+    db.insert_one(dict(prod))
+    return prodEntity(db.find_one({"name": prod.name}))
+
+@product.put('/{id}')
+async def update_user(id, prod: Product):
+    db.find_one_and_update({"_id": objectid(id)},{"$set":dict(prod)})
+    return prodEntity(db.find_one({"_id": objectid(id)}))
+
+@product.delete('/{id}')
+async def delete_user(id, prod: Product):
+    db.find_one_and_delete({"_id": objectid(id)})
+    return {"message": "User deleted succesfully"}
